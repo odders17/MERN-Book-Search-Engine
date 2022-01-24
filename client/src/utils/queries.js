79c6477d6b86fd417d464f8c77fd3d3989192c3e
odders17 <odders17@gmail.com>
2022-01-24 12:02:09 +0000

    uses graphql

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,20 +1,79 @@
-import gql from "graphql-tag";
+import { gql } from "@apollo/client";
 
-export const GET_ME  = gql`
-    {
-        me {
-            _id
-            username
-            email
-            bookCount
-            savedBooks {
-                bookId
-                authors
-                title
-                description
-                image
-                link
-            }
-        }
+export const GET_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        description
+        image
+        link
+        title
+      }
+  }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
     }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($bookToSave: BookToSave!) {
+    saveBook(book: $bookToSave) {
+      _id
+      email
+      username
+      savedBooks {
+        bookId
+        authors
+        description
+        image
+        link
+        title
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      _id
+      email
+      username
+      savedBooks{
+        bookId
+        authors
+        description
+        image
+        title
+        link
+      }
+    }
+  }
+`;
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
 `;
