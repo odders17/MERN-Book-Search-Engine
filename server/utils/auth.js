9bb3a10ae3502cd08ee5b0e6c343f7811d61f74b
odders17 <odders17@gmail.com>
2022-01-25 07:21:32 +0000

    bug fixes

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 // set token secret and expiration date
-const secret = 'MySecretShhhhhh';
+const secret = 'SecretRandomSalt';
 const expiration = '2h';
 
 module.exports = {
