259b0766f969100e77f6e323b96c6856e5756400
odders17 <odders17@gmail.com>
2022-01-25 08:04:00 +0000

    started again as something was broken

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 // set token secret and expiration date
-const secret = 'SecretRandomSalt';
+const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
@@ -16,38 +16,24 @@ module.exports = {
     }
 
     if (!token) {
-      return req;
+      return res.status(400).json({ message: 'You have no token!' });
     }
 
     // verify token and get user data out of it
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      console.log("Added user: ", data)
       req.user = data;
     } catch {
       console.log('Invalid token');
-
+      return res.status(400).json({ message: 'invalid token!' });
     }
-    return req;
 
+    // send to next endpoint
+    next();
   },
-  signToken: function ({
-    username,
-    email,
-    _id
-  }) {
-    const payload = {
-      username,
-      email,
-      _id
-    };
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
 
-    return jwt.sign({
-      data: payload
-    },
-      secret,
-      {
-        expiresIn: expiration
-      });
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
